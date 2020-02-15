19fa59ace5e9380efe7ead9ddfe6d2ff8f2925bd
Test <mattatatter@gmail.com>
2020-02-15 15:28:06 -0600

    accepted changes in mlt.ino

--- mlt.ino
+++ mlt.ino
@@ -1,23 +1,42 @@
-#include <Wire.h>
-//#include <SoftwareSerial.h>
+#include "SevSeg.h"
+
+SevSeg sevseg;
+
+unsigned long before_time = 0;
+const long SECOND = 1000;
+const int frequency = 1;
+const long SAMPLE_TIME = SECOND / frequency;
 
 const int MIC1 = A0;
 
 void setup(){
+
     pinMode(MIC1, INPUT);
 
-    Serial.begin(57600);
-    Wire.begin();
-    
+    byte num_digits = 4;
+    // byte digit_pins[] = {10, 11, 12, 13};
+    byte digit_pins[] = {13, 12, 11, 10};
+    byte segment_pins[] = {2, 3, 4, 5, 6, 7, 8, 9};
+
+    bool resistors_on_segments = true;
+    bool update_with_delays_in = false;
+    bool leading_zeros = true;
+    byte hardware_config = COMMON_CATHODE;
+    sevseg.begin(hardware_config, num_digits, digit_pins, segment_pins, resistors_on_segments, update_with_delays_in, leading_zeros);
+    sevseg.setBrightness(90);
+
+    before_time = millis();
 }
 
 void loop(){
 
-    int x = analogRead(MIC1);
-    Serial.print("x: ");
-    Serial.println(x);
+    unsigned long elapsed = millis() - before_time;
+    if(elapsed >= SAMPLE_TIME){
 
-    //change value range from 10 bit to 8 bit
-    map(x, 0, 1023, 0, 255);
+        int signal_counter = analogRead(MIC1);
+        sevseg.setNumber(signal_counter);
+        before_time += SAMPLE_TIME;
+    }
 
+    sevseg.refreshDisplay();
 }
