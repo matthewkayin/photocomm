beeaab0cb5035d877a51ce0c8e9152ea7c7407dd
Matthew Drummond <mattatatter@gmail.com>
2020-02-20 11:27:42 -0600

    added sine wave generator for testing purposes

--- tests/ardTest.cpp
+++ tests/ardTest.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 #include "serial.hpp"
 
+//constants for sine wave
+const float AMP = 10;
+const float FREQ = 100;
+const float PHASE = 0.0;
+const int SAMPLE_RATE = 44100;
+const float TIME_CHANGE = 1.0 / SAMPLE_RATE;
+const int BUFFSIZE = 500;
 
 void compare(int num1, int num2);
+void sineWaveGen(double* arr, int size);
 
 int main(){
 
@@ -15,19 +24,25 @@ int main(){
     std::cout << *msg << std::endl;
 
     
-    for (int i = 0; i < 1024; i++){
-        std::cout << i << std::endl;
-        if(opened){
-            sent = i;
-            arduino.write(sent);
-            rec = arduino.read();
-            compare(sent, rec);
+
+    double *monoBuffer = new double[BUFFSIZE];
+     
+    sineWaveGen(monoBuffer, BUFFSIZE);
+
+    //if(opened){
+        for (int i = 0; i < BUFFSIZE; i++){
+        std::cout << monoBuffer[i] << std::endl;
+
+        //arduino.write(monoBuffer[i]);
+        //rec = arduino.read();
+        //compare(sent, rec);
         }
-        else{
-            //TODO: deal with unopened serial
-            //break;
-        } 
-    }
+
+    //}
+    //else{
+        //TODO: deal with unopened serial
+    //} 
+    
     return 0;
 }
 
@@ -37,7 +52,21 @@ void compare(int sent, int received){
     }
     else{
         std::cout << "TEST FAILED!" << std::endl;
-        std::cout << "SENT : " << sent;
+        std::cout << "SENT : " << sent << '\t';
         std::cout << "RECEIVED: " << received << std::endl;
+    }
+}
+
+void sineWaveGen(double* arr, int size){
+
+    float time = 0.0;
+    
+    for (int sample = 0; sample < size; sample++){
+        
+        double value = AMP * sin(2 * (2 * M_PI) * FREQ * time + PHASE);
+        arr[sample] = value;
+        
+        time += TIME_CHANGE;
+           
     }
 }
