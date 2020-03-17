39185d8af8f16760bf990bb46b6214bf2ebf4926
Angel O. Lopez <32178007+AngelLopez8@users.noreply.github.com>
2020-03-17 14:33:18 -0500

    Update binaryfunctions.cpp

--- tests/binaryfunctions.cpp
+++ tests/binaryfunctions.cpp
@@ -1,7 +1,10 @@
 bool* to_binary(int n, int r){
 	bool *bin = new bool[r];
 	int i;
-	for(i = 0; n > 0; i++){
+	if(n < 0){
+		bin[0] = 1;
+	} else { bin[0] = 0; }
+	for(i = 1; n > 0; i++){
 		bin[i] = n % 2;
 		n = n /2;
 	}
@@ -14,8 +17,11 @@ bool* to_binary(int n, int r){
 
 int to_int(bool* n, int r){
 	int num = 0, i;
-	for(i = 0; i < r; i++){
+	for(i = 0; i < r-1; i++){
 		num += (n[i] * pow(2, i));
+	}
+	if(n[r-1] == 1){
+		return num * -1;
 	}
 	return num;
 }
