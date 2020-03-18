751949b935e8d4c364bd7469a619631d31f3ff87
matthewkayin <matthewkayin@gmail.com>
2020-03-18 10:49:23 -0500

    Added test infrastructure to binary functions
    Signed-off-by: matthewkayin <matthewkayin@gmail.com>

--- tests/binaryfunctions.cpp
+++ tests/binaryfunctions.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <math.h>
+
 bool* to_binary(int n, int r){
 	bool *bin = new bool[r];
 	int i;
@@ -6,7 +9,7 @@ bool* to_binary(int n, int r){
 		n = n /2;
 	}
 	if(i > r){
-		cout << "ERROR OUT OF RANGE!";
+        std::cout << "ERROR OUT OF RANGE!";
 		return NULL;
 	}
 	return bin;
@@ -15,7 +18,23 @@ bool* to_binary(int n, int r){
 int to_int(bool* n, int r){
 	int num = 0, i;
 	for(i = 0; i < r; i++){
-		num += (n[i] * pow(2, i));
+		num += (n[i] * std::pow(2, i));
 	}
 	return num;
+}
+
+int main(){
+
+    int n = 2;
+    bool* output = to_binary(n, 8);
+    for(int i = 0; i < 8; i++){
+
+        std::cout << output[i];
+    }
+    std::cout << std::endl;
+    bool success = to_int(output, 8) == n;
+
+    std::cout << success << std::endl;
+
+    return 0;
 }
